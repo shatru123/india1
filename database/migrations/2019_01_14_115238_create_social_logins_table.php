41f2fdace8433c3b1f35d1693f6a344d54b5369e
Shatrughna Ambhore <ambhoreshatrughna@gmail.com>
2019-01-14 17:50:54 +0530

    social_login

---
+++ database/migrations/2019_01_14_115238_create_social_logins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialLoginsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_logins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('google_id ')->nullable();
+            $table->string('password ')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_logins');
+    }
+}
